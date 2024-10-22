fce7a3988eee8766ff321dc5c10d9d6c2041d32f
Ubuntu <ubuntu@ip-172-31-54-248.ec2.internal>
2024-10-22 11:24:06 +0000

    test

---
+++ app/controllers/top_controller.rb
@@ -0,0 +1,46 @@
+class TopController < ApplicationController
+    def main
+        if session[:login_uid]
+            redirect_to tweets_path
+        else
+            render "login"
+        end
+    end
+    
+    #def login
+        #if User.find_by(uid: params[:uid])and User.find_by(uid: params[:pass])
+          #  session[:login_uid]=params[:uid]
+           # redirect_to top_main_path
+       # else
+          #  render "error",status: 422
+       # end
+   # end
+    
+    #def login
+       # user = User.find_by(uid: params[:uid])
+       # if user && user.pass == params[:pass]  # ここでパスワードを比較
+      #  session[:login_uid] = user.uid
+      #  redirect_to top_main_path
+      #  else
+      #      render "error", status: 422
+      #  end
+   # end
+
+    
+    def login
+        user=User.find_by(uid: params[:uid])
+        if user && BCrypt::Password.new(user.pass) == params[:pass] 
+           session[:login_uid]=params[:uid]
+            redirect_to top_main_path
+        else
+            render "error", status: 422
+        end
+    end
+
+    def logout
+        session.delete(:login_uid)
+        redirect_to root_path
+    end
+    
+    
+end
