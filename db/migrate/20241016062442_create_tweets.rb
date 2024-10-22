df4e198bca4e8ef6e8fa7129451dbf70933af103
Ubuntu <ubuntu@ip-172-31-54-248.ec2.internal>
2024-10-22 11:48:10 +0000

    push to render.com

--- db/migrate/20241016062442_create_tweets.rb
+++ db/migrate/20241016062442_create_tweets.rb
@@ -2,7 +2,7 @@ class CreateTweets < ActiveRecord::Migration[7.1]
   def change
     create_table :tweets do |t|
       t.string :message
-      t.integer:user_id
+      t.integer :user_id
 
       t.timestamps
     end
